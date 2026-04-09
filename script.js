c44bb89084e155b166cf6576f8983060020b3281
Okan ALPAY <alpaydevv@gmail.com>
2026-04-09 23:04:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const diceBtn = document.querySelector(".roll-btn");
+
+diceBtn.addEventListener("click", rollDice);
+
+function rollDice() {
+  const random1 = Math.floor(Math.random() * 6) + 1;
+  const random2 = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector(".img1").setAttribute("src", `images/dice${random1}.png`);
+  document.querySelector(".img2").setAttribute("src", `images/dice${random2}.png`);
+
+  if (random1 > random2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  } else if (random2 > random1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+}
